eb9018308284702c1e253284f4031c6e1f7f9ce2
jisilver <jisilver.kim@gmail.com>
2024-01-03 00:14:32 +0900

    Design: Landing 페이지 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,98 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidde rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all  hover:border-gray-300 hover:bg-white/50">
-        <p className="text-sm font-semibold text-gray-700">Hello</p>
+    <>
+      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        {/* desc */}
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          내 <span className="text-blue-600">이력서</span>로 AI 면접관 만나기
+        </h1>
+        <p className="mt-5 max-w-prose text-zinc-700">내 이력서 파일을 업로드해서 GPT가 예상 질문을 뽑아주며 실전 면접 대비를 할 수 있어요!</p>
+
+        {/* button */}
+        <Link className={buttonVariants({
+          size: 'lg',
+          className: 'mt-5'
+        })} href='/dashboard' target='_blank'>시작하기 <ArrowRight className="ml-2 h-5 w-5" /></Link>
+      </MaxWidthWrapper>
+
+
+      <div>
+        <div className="relative isolate">
+
+          {/* background */}
+          <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+            <div style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }} className="relative left-[calc(50% - 11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" />
+          </div>
+
+          {/* preview image */}
+          <div>
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image src='/dashboard-preview.jpg' alt="preview" width={1364} height={866} quality={100} className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+            <div style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }} className="relative left-[calc(50% - 13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]" />
+          </div>
+        </div>
       </div>
-    </MaxWidthWrapper>
+
+      {/* steps */}
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">사용 방법</h2>
+          </div>
+        </div>
+
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Step1</span>
+              <span className="text-xl font-semibold">가입하기</span>
+              <span className="mt-2 text-zinc-700">무료로 시작하거나 <Link href='/pricing' className="text-blue-700 underline underline-offset-2">멤버십</Link>을 이용해보세요.</span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Step2</span>
+              <span className="text-xl font-semibold">PDF 파일 업로드</span>
+              <span className="mt-2 text-zinc-700">GPT가 PDF파일을 분석해서 예상질문을 드립니다.</span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Step3</span>
+              <span className="text-xl font-semibold">대화하기</span>
+              <span className="mt-2 text-zinc-700">예상질문을 받고 실제 면접을 보는 것처럼 대화를 해보세요!</span>
+            </div>
+          </li>
+        </ol>
+
+        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <Image src='/file-upload-preview.jpg' alt="preview" width={1419} height={732} quality={100} className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,6 @@
-import clsx, { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-// clsx로 클래스값 조합하기(여러 클래스를 받아서 하나의 문자열로 합치기)
-// twMerge()로 TailwindCSS 클래스 병합 후 중복된 클래스 제거
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
